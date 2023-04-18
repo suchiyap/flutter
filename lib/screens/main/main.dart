24086ffe5e21414cbe52f093b698964d027e79c3
William <williamwql@hotmail.com>
2023-04-18 17:53:33 +0800

    close app confirmation

--- lib/screens/main/main.dart
+++ lib/screens/main/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:temple_dev/constants.dart';
 import 'package:temple_dev/components/nav_bar.dart';
 import 'package:temple_dev/screens/home/home_screen.dart';
 
@@ -13,9 +14,39 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: MyHomePage(title: '海南会東禪寺'),
-      bottomNavigationBar: NavBar(),
+    Future<bool> showCloseAppDialog() async {
+      return await showDialog(
+        context: context,
+        barrierDismissible: true, // user must tap button
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: kPrimaryBackgroundColor,
+            // title: const Text('Close APP'),
+            content: const Text('Are you sure you want to close application?', style: TextStyle(fontSize: 14)),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('No', style: TextStyle(color: kSubTextColor)),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              TextButton(
+                child: const Text('Yes'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+    return WillPopScope(
+      onWillPop: showCloseAppDialog,
+      child: const Scaffold(
+        body: MyHomePage(title: '海南会東禪寺'),
+        bottomNavigationBar: NavBar(),
+      ),
     );
   }
 }
