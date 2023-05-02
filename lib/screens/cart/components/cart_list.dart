16596412f415bb9f22258028f3226ea2772e0b89
suchiyap <suchi@iscity.com.my>
2023-05-02 14:21:52 +0800

    Update cart_list.dart

--- lib/screens/cart/components/cart_list.dart
+++ lib/screens/cart/components/cart_list.dart
@@ -23,60 +23,65 @@ class _CartListingState extends State<CartListing> {
 
   @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(
-      activeColor: kTextColor,
-      contentPadding:
-          const EdgeInsets.only(top: 10, left: 0, bottom: 10, right: 10),
-      value: _isSelected,
-      onChanged: (newValue) {
-        setState(() {
-          _isSelected = newValue!;
-        });
-      },
-      title: Container(
-        margin: const EdgeInsets.only(left: 0),
-        width: double.infinity,
+    return Padding(
+        padding: const EdgeInsets.all(10),
         child: Row(
           children: [
-            Container(
-              width: (kIsWeb ? kWebWidth : MediaQuery.of(context).size.width) *
-                  0.25,
-              padding: const EdgeInsets.only(right: 5),
-              child: Image.asset(
-                'assets/images/product/Product 1.png',
-                // width: 100,
-                // height: 100,
-              ),
-            ),
+            Checkbox(
+                activeColor: kTextColor,
+                checkColor: Colors.white,
+                value: _isSelected,
+                onChanged: (value) {
+                  setState(() => _isSelected = value!);
+                }),
             Expanded(
-                child: SizedBox(
-                    height: 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          '清明节配套 拜祖先配套 清明配套 清明祭品 扫墓配套 清明神料 祭祖....',
-                          style: TextStyle(
-                              fontSize: 13, fontWeight: FontWeight.bold),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: const [
-                            AddMinusQty(count: 1),
-                            Text(
-                              'RM 148.00',
-                              style: TextStyle(
-                                  color: kErrorColor,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        )
-                      ],
-                    )))
+                child: Container(
+              margin: const EdgeInsets.only(left: 0),
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Container(
+                    width: (kIsWeb
+                            ? kWebWidth
+                            : MediaQuery.of(context).size.width) *
+                        0.25,
+                    padding: const EdgeInsets.only(right: 5),
+                    child: Image.asset(
+                      'assets/images/product/Product 1.png',
+                      // width: 100,
+                      // height: 100,
+                    ),
+                  ),
+                  Expanded(
+                      child: SizedBox(
+                          height: 100,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '清明节配套 拜祖先配套 清明配套 清明祭品 扫墓配套 清明神料 祭祖....',
+                                style: TextStyle(
+                                    fontSize: 13, fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  AddMinusQty(count: 1),
+                                  Text(
+                                    'RM 148.00',
+                                    style: TextStyle(
+                                        color: kErrorColor,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              )
+                            ],
+                          )))
+                ],
+              ),
+            ))
           ],
-        ),
-      ),
-      controlAffinity: ListTileControlAffinity.leading,
-    );
+        ));
   }
 }
